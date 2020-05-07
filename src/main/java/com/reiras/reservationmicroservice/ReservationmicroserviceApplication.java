6ddbd58c1d9bf29f8c425bfe920d0c43254c40f1
reirasdev <reiras.deve@gmail.com>
2020-05-07 02:13:17 -0300

    Initial commit - Project created

---
+++ src/main/java/com/reiras/reservationmicroservice/ReservationmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.reiras.reservationmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationmicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reiras/reservationmicroservice/ReservationmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reiras.reservationmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
